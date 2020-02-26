4a33c228d746c1272abf63f45e52fa24cb8af594
M@St3r_iLLuSioN <illusiongrouppcs@gmail.com>
2020-02-26 17:44:58 -0500

    v0.0.21 Release

--- Controller/WikiPageController.php
+++ Controller/WikiPageController.php
@@ -166,42 +166,4 @@ class WikiPageController extends WikiBaseController
             'form' => $form->createView(),
         ]);
     }
-
-    protected function getWikiPermission(Wiki $wiki)
-    {
-        $wikiRoles = ['readRole' => false, 'writeRole' => false];
-        $flag = false;
-
-        if ($this->isGranted('ROLE_SUPERUSER')) {
-            $wikiRoles['readRole'] = true;
-            $wikiRoles['writeRole'] = true;
-            $flag = true;
-        } else {
-            if (!empty($wiki->getReadRole())) {
-                $readArray = explode(',', $wiki->getReadRole());
-                array_walk($readArray, 'trim');
-
-                foreach ($readArray as $read) {
-                    if ($this->isGranted($read)) {
-                        $wikiRoles['readRole'] = true;
-                        $flag = true;
-                    }
-                }
-            }
-
-            if (!empty($wiki->getWriteRole())) {
-                $writeArray = explode(',', $wiki->getWriteRole());
-                array_walk($writeArray, 'trim');
-
-                foreach ($writeArray as $write) {
-                    if ($this->isGranted($write)) {
-                        $flag = true;
-                        $wikiRoles['writeRole'] = true;
-                    }
-                }
-            }
-        }
-
-        return  $flag ? $wikiRoles : false;
-    }
 }
